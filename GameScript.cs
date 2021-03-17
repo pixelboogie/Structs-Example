102aba9da966d85744088c9a9987a099e4e0cb30
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-17 08:31:06 -0700

    Add files via upload

---
+++ GameScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+
+    Hero myHero;
+    
+    void Start()
+    {
+        myHero = new Hero();
+        myHero.Name = "Elvis";
+        myHero.Score = 100;
+        myHero.Lives = 3;
+    }
+
+
+    void OnDisable()
+    {
+        Debug.Log("Name: " + myHero.Name + ", Score: " + myHero.Score);
+    }
+}
---
+++ Hero.cs
@@ -0,0 +1,7 @@
+﻿public struct Hero
+{
+    public string Name;
+    public int Score;
+    public int Lives;
+
+}
